19e75f14146dc5de2a29466dac7c1ec4a03d8df7
onoche win8 <onozuka69@gmail.com>
2017-04-10 21:58:14 +0900

    ボタン、テキストボックス追加

--- src/v-ir/NdkPrj/app/src/main/java/com/example/admin/vir/MainActivity.java
+++ src/v-ir/NdkPrj/app/src/main/java/com/example/admin/vir/MainActivity.java
@@ -3,17 +3,38 @@ package com.example.admin.vir;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
+
+import com.example.admin.vir.R.id;
+
+import static com.example.admin.vir.R.id.sample_text;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+        // Example of a call to a native method
+        tv = (TextView) findViewById(sample_text);
+        tv.setText(stringFromJNI());
+
+        Button button = (Button) findViewById(id.button);
+        // ボタンがクリックされた時に呼び出されるコールバックリスナーを登録します
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ボタンがクリックされた時に呼び出されます
+                Button button = (Button) v;
+//                Toast.makeText(ButtonSampleActivity.this, "onClick()",
+//                        Toast.LENGTH_SHORT).show();
+                tv.setText(tv.getText() + "\n" + "push");
+            }
+        });
     }
 
     /**
